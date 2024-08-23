921512d52bbe8620f3b2ba171d74cab0803a7c1a
Zohaib Rauf <zabirauf@gmail.com>
2024-08-23 01:23:19 -0700

    Just small changes, nothing actual of value

--- src/main.cu
+++ src/main.cu
@@ -2,23 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CHECK_CUDA_ERROR() do { \
-    cudaError_t error = cudaGetLastError(); \
-    if (error != cudaSuccess) { \
-        fprintf(stderr, "File: %s, Line: %d, CUDA error: %s\n", __FILE__, __LINE__ , cudaGetErrorString(error)); \
-        exit(-1); \
-    } \
-} while(0)
+#define CHECK_CUDA_ERROR()                                                                                          \
+    do                                                                                                              \
+    {                                                                                                               \
+        cudaError_t error = cudaGetLastError();                                                                     \
+        if (error != cudaSuccess)                                                                                   \
+        {                                                                                                           \
+            fprintf(stderr, "File: %s, Line: %d, CUDA error: %s\n", __FILE__, __LINE__, cudaGetErrorString(error)); \
+            exit(-1);                                                                                               \
+        }                                                                                                           \
+    } while (0)
 
-__global__ void add_kernel(float *a, float *b, float *c, uint n) {
+__global__ void add_kernel(float *a, float *b, float *c, uint n)
+{
     uint idx = threadIdx.x;
 
-    if (idx < n) {
+    if (idx < n)
+    {
         c[idx] = a[idx] + b[idx];
     }
 }
 
-int main() {
+int main()
+{
     // Select the GPU you want to use (0 for the first GPU, 1 for the second, etc.)
     // int deviceId = 0; // Change this to 1 if you want to use the second GPU
     // cudaError_t error = cudaSetDevice(deviceId);
@@ -28,7 +34,9 @@ int main() {
     // }
 
     // Allocate host memory and initialize data
-    float *a_h = (float *)malloc(10 * sizeof(float));
+    uint n = 10;
+    uint size = n * sizeof(float);
+    float *a_h = (float *)malloc(size);
     a_h[0] = 1.0;
     a_h[1] = 2.0;
     a_h[2] = 3.0;
@@ -40,7 +48,7 @@ int main() {
     a_h[8] = 9.0;
     a_h[9] = 10.0;
 
-    float *b_h = (float *)malloc(10 * sizeof(float));
+    float *b_h = (float *)malloc(size);
     b_h[0] = 10.0;
     b_h[1] = 11.0;
     b_h[2] = 12.0;
@@ -55,28 +63,37 @@ int main() {
     // Allocate device memory and copy data from host to device
     float *a_d, *b_d, *c_d;
 
-    cudaMalloc((void**)&a_d, 10 * sizeof(float)); CHECK_CUDA_ERROR();
-    cudaMalloc((void**)&b_d, 10 * sizeof(float)); CHECK_CUDA_ERROR();
-    cudaMalloc((void**)&c_d, 10 * sizeof(float)); CHECK_CUDA_ERROR();
-
-    cudaMemcpy(a_d, a_h, 10 * sizeof(float), cudaMemcpyHostToDevice); CHECK_CUDA_ERROR();
-    cudaMemcpy(b_d, b_h, 10 * sizeof(float), cudaMemcpyHostToDevice); CHECK_CUDA_ERROR();
-
-    add_kernel<<<1, 10>>>(a_d, b_d, c_d, 10);
-    
+    cudaMalloc((void **)&a_d, size);
+    CHECK_CUDA_ERROR();
+    cudaMalloc((void **)&b_d, size);
+    CHECK_CUDA_ERROR();
+    cudaMalloc((void **)&c_d, size);
     CHECK_CUDA_ERROR();
 
-    float *c_h = (float *)malloc(10 * sizeof(float));
-    cudaMemcpy(c_h, c_d, 10 * sizeof(float), cudaMemcpyDeviceToHost); CHECK_CUDA_ERROR();
+    cudaMemcpy(a_d, a_h, size, cudaMemcpyHostToDevice);
+    CHECK_CUDA_ERROR();
+    cudaMemcpy(b_d, b_h, size, cudaMemcpyHostToDevice);
+    CHECK_CUDA_ERROR();
 
-    for (int i = 0; i < 10; i++) {
+    add_kernel<<<1, 10>>>(a_d, b_d, c_d, n);
+    CHECK_CUDA_ERROR();
+
+    float *c_h = (float *)malloc(size);
+    cudaMemcpy(c_h, c_d, size, cudaMemcpyDeviceToHost);
+    CHECK_CUDA_ERROR();
+
+    for (int i = 0; i < n; i++)
+    {
         printf("%f\n", c_h[i]);
     }
 
     // Cleanup device memory
-    cudaFree(a_d); CHECK_CUDA_ERROR();
-    cudaFree(b_d); CHECK_CUDA_ERROR();
-    cudaFree(c_d); CHECK_CUDA_ERROR();
+    cudaFree(a_d);
+    CHECK_CUDA_ERROR();
+    cudaFree(b_d);
+    CHECK_CUDA_ERROR();
+    cudaFree(c_d);
+    CHECK_CUDA_ERROR();
 
     // Cleanup host memory
     free(a_h);
