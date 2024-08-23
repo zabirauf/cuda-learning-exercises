59cc7032d9c765935d314080ca9cda575fba9f2b
Zohaib Rauf <zabirauf@gmail.com>
2024-08-23 01:21:09 -0700

    Aggressive cuda error checking :)

--- src/main.cu
+++ src/main.cu
@@ -5,7 +5,7 @@
 #define CHECK_CUDA_ERROR() do { \
     cudaError_t error = cudaGetLastError(); \
     if (error != cudaSuccess) { \
-        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(error)); \
+        fprintf(stderr, "File: %s, Line: %d, CUDA error: %s\n", __FILE__, __LINE__ , cudaGetErrorString(error)); \
         exit(-1); \
     } \
 } while(0)
@@ -27,6 +27,7 @@ int main() {
     //     return 1;
     // }
 
+    // Allocate host memory and initialize data
     float *a_h = (float *)malloc(10 * sizeof(float));
     a_h[0] = 1.0;
     a_h[1] = 2.0;
@@ -51,32 +52,33 @@ int main() {
     b_h[8] = 18.0;
     b_h[9] = 19.0;
 
-
-
+    // Allocate device memory and copy data from host to device
     float *a_d, *b_d, *c_d;
 
-    cudaMalloc((void**)&a_d, 10 * sizeof(float));
-    cudaMalloc((void**)&b_d, 10 * sizeof(float));
-    cudaMalloc((void**)&c_d, 10 * sizeof(float));
+    cudaMalloc((void**)&a_d, 10 * sizeof(float)); CHECK_CUDA_ERROR();
+    cudaMalloc((void**)&b_d, 10 * sizeof(float)); CHECK_CUDA_ERROR();
+    cudaMalloc((void**)&c_d, 10 * sizeof(float)); CHECK_CUDA_ERROR();
 
-    cudaMemcpy(a_d, a_h, 10 * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(b_d, b_h, 10 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(a_d, a_h, 10 * sizeof(float), cudaMemcpyHostToDevice); CHECK_CUDA_ERROR();
+    cudaMemcpy(b_d, b_h, 10 * sizeof(float), cudaMemcpyHostToDevice); CHECK_CUDA_ERROR();
 
     add_kernel<<<1, 10>>>(a_d, b_d, c_d, 10);
     
     CHECK_CUDA_ERROR();
 
     float *c_h = (float *)malloc(10 * sizeof(float));
-    cudaMemcpy(c_h, c_d, 10 * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c_h, c_d, 10 * sizeof(float), cudaMemcpyDeviceToHost); CHECK_CUDA_ERROR();
 
     for (int i = 0; i < 10; i++) {
         printf("%f\n", c_h[i]);
     }
 
-    cudaFree(a_d);
-    cudaFree(b_d);
-    cudaFree(c_d);
+    // Cleanup device memory
+    cudaFree(a_d); CHECK_CUDA_ERROR();
+    cudaFree(b_d); CHECK_CUDA_ERROR();
+    cudaFree(c_d); CHECK_CUDA_ERROR();
 
+    // Cleanup host memory
     free(a_h);
     free(b_h);
     free(c_h);
